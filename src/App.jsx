803e9cf14faee74d5ec111bbdfd9df2aada8c4d5
alkanaziz <alkanaziz@gmail.com>
2024-04-19 11:13:01 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <p>A list of frameworks</p>
+      <ul>
+        <li>data comes from backend</li>
+      </ul>
+    </>
+  )
+}
+
+export default App
